32759860b9884213a29ea26345f51a6428c30476
David Wilson <david.tosslehoff@gmail.com>
2014-11-30 20:25:26 +0000

    Validation done for now.

--- Interactors/UpdatePlatformInteractor.py
+++ Interactors/UpdatePlatformInteractor.py
@@ -3,4 +3,11 @@ from Interactors.Interactor import Interactor
 
 class UpdatePlatformInteractor(Interactor):
     def execute(self, platform):
+
+        if platform is None:
+            raise TypeError("platform")
+
+        if platform.name is None or platform.name.strip() == "":
+            raise ValueError("Platform name must be set")
+
         self.persistence.update_platform(platform)
--- Tests/Interactors/TestUpdatePlatformInteractor.py
+++ Tests/Interactors/TestUpdatePlatformInteractor.py
@@ -16,10 +16,24 @@ class TestUpdatePlatformInteractor(unittest.TestCase):
     def test_is_instance_of_interactor(self):
         self.assertIsInstance(self.__target, Interactor)
 
-    def test_execute(self):
-        self.__target.execute(platform=Platform())
-
     def test_execute_calls_persistence_update_platform(self):
-        platform = Platform()
+        platform = self.__get_platform()
         self.__target.execute(platform)
         self.__persistence.update_platform.assert_called_with(platform)
+
+    def test_execute_with_none_platform_raises_type_error(self):
+        self.assertRaises(TypeError, self.__target.execute, None)
+
+    def test_execute_with_none_name_raises_value_error(self):
+        self.assertRaises(ValueError, self.__target.execute, self.__get_platform(name=None))
+
+    def test_execute_with_empty_name_raises_value_error(self):
+        self.assertRaises(ValueError, self.__target.execute, self.__get_platform(name=""))
+
+    def test_execute_with_whitespace_name_raises_value_error(self):
+        self.assertRaises(ValueError, self.__target.execute, self.__get_platform(name=" "))
+
+    def __get_platform(self, name="name"):
+        p = Platform()
+        p.name = name
+        return p
