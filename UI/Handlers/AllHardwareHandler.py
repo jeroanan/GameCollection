6743a3432e7db8d41c969a974557c143c195441c
David Wilson <david.tosslehoff@gmail.com>
2015-03-23 21:16:26 +0000

    Ajaxify all hardware page sort

--- UI/Handlers/AllHardwareHandler.py
+++ UI/Handlers/AllHardwareHandler.py
@@ -5,4 +5,5 @@ class AllHardwareHandler(Handler):
     def get_page(self, args):
         interactor = self.interactor_factory.create("GetHardwareListInteractor")
         hardware = interactor.execute(sort_field="name", sort_direction="asc")
-        return self.renderer.render("allhardware.html", hardware=hardware, title="All Hardware")
+        return self.renderer.render("allhardware.html", hardware=hardware, title="All Hardware",
+                                    hw_sort_field="name", hw_sort_dir="asc")
