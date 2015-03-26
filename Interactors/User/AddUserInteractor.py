25861844db64f1a4b5afc304435619343e6a2dc1
David Wilson <david.tosslehoff@gmail.com>
2015-03-26 22:42:44 +0000

    Basic add user interactor

--- Interactors/User/AddUserInteractor.py
+++ Interactors/User/AddUserInteractor.py
@@ -9,15 +9,15 @@ class AddUserInteractor(Interactor):
         self.__stop_if_user_exists(user)
         self.persistence.add_user(user)
 
+    def __validate(self, user):
+        if user is None:
+            raise TypeError
+        self.validate_string_field("user_id", user.user_id)
+        self.validate_string_field("password", user.password)
+
     def __stop_if_user_exists(self, user):
         if self.__user_exists(self.persistence.get_user(user)):
             raise UserExistsException
 
     def __user_exists(self, user):
         return len(list(user)) > 0
-
-    def __validate(self, user):
-        if user is None:
-            raise TypeError
-        self.validate_string_field("user_id", user.user_id)
-        self.validate_string_field("password", user.password)
