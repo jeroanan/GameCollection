2c3f13713d8bb3b9538aaacc0928a2204461d27c
David Wilson <david.tosslehoff@gmail.com>
2015-03-28 21:12:22 +0000

    Case sensitivity

--- Data/Config.py
+++ Data/Config.py
@@ -4,7 +4,7 @@ import json
 class Config(object):
 
     def __init__(self):
-        with open("Data/Config.json") as f:
+        with open("Data/config.json") as f:
             data = json.load(f)
             self.__data = data["config"]
 
