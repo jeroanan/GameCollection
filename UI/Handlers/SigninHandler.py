528cc2f1b6b0c0f597e5c6e341658e095c6ae41a
David Wilson <david.tosslehoff@gmail.com>
2015-03-30 23:13:55 +0100

    Hooked up loginhandler

---
+++ UI/Handlers/SigninHandler.py
@@ -0,0 +1,17 @@
+from Cryptography.BCryptHashProvider import BCryptHashProvider
+from UI.Handlers.Handler import Handler
+from User import User
+
+
+class SigninHandler(Handler):
+    
+    def get_page(self, params):
+        interactor = self.interactor_factory.create("LoginInteractor")
+        interactor.set_hash_provider(BCryptHashProvider())
+        return interactor.execute(self.__get_user(params))
+
+    def __get_user(self, params):
+        u = User()
+        u.user_id = params.get("userid", "")
+        u.password = params.get("password", "")
+        return u
---
+++ UI/Tests/Handlers/TestSigninHandler.py
@@ -0,0 +1,42 @@
+import unittest
+from unittest.mock import Mock
+
+from Cryptography.HashProvider import HashProvider
+from Interactors.InteractorFactory import InteractorFactory
+from Interactors.User.LoginInteractor import LoginInteractor
+from UI.Handlers.Handler import Handler
+from UI.Handlers.SigninHandler import SigninHandler
+from User import User
+
+
+class TestSigninHandler(unittest.TestCase):
+    
+    def setUp(self):
+        self.__interactor = Mock(LoginInteractor)
+        self.__hash_provider = Mock(HashProvider)
+        interactor_factory = Mock(InteractorFactory)
+        interactor_factory.create = Mock(return_value=self.__interactor)
+        self.__target = SigninHandler(interactor_factory, None)
+        
+    def test_is_handler(self):
+        self.assertIsInstance(self.__target, Handler)
+
+    def test_get_page_sets_interactor_hash_provider(self):
+        self.__target.get_page(self.__get_params())
+        self.assertTrue(self.__interactor.set_hash_provider.called)
+    
+    def test_get_page_executes_interactor(self):
+        self.__target.get_page(self.__get_params())
+        self.__interactor.execute.assert_called_with(self.__get_user())
+        
+    def __get_params(self):
+        return {
+            "userid": "userid",
+            "password": "password"
+            }
+
+    def __get_user(self):
+        u = User()
+        u.user_id = "userid"
+        u.password = "password"
+        return u
