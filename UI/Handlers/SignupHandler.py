2a6cd41cf0307ed8638279ccee08c4666c468778
David Wilson <david.tosslehoff@gmail.com>
2015-04-03 22:31:41 +0100

    Signup now causes login

--- UI/Handlers/SignupHandler.py
+++ UI/Handlers/SignupHandler.py
@@ -3,19 +3,33 @@ from Interactors.Exceptions.UserExistsException import UserExistsException
 from User import User
 from UI.Handlers.Handler import Handler
 
-
 class SignupHandler(Handler):
-    def get_page(self, params):
-        interactor = self.interactor_factory.create("AddUserInteractor")
-        interactor.set_hash_provider(BCryptHashProvider())
+    def get_page(self, params):        
+        self.check_session()
+        u = self.__get_user_from_params(params)        
         try:
-            interactor.execute(self.__get_user(params.get("userid", ""), params.get("password", "")))
+            self.__add_user(u)
         except UserExistsException:
-            pass
+            return str(True)
+
+        self.__do_login(u)
         return str(True)
         
+    def __get_user_from_params(self, params):
+        return self.__get_user(params.get("userid", ""), params.get("password", ""))        
+
     def __get_user(self, user_id, password):
         u = User()
         u.user_id = user_id
         u.password = password        
         return u
+
+    def __add_user(self, user):
+        add_user_interactor = self.interactor_factory.create("AddUserInteractor")
+        add_user_interactor.set_hash_provider(BCryptHashProvider())
+        add_user_interactor.execute(user)        
+
+    def __do_login(self, user):
+        interactor = self.interactor_factory.create("LoginInteractor")
+        interactor.execute(user)
+        self.session.set_value("user_id", user.user_id)
--- UI/Tests/Handlers/TestSignupHandler.py
+++ UI/Tests/Handlers/TestSignupHandler.py
@@ -3,7 +3,10 @@ from unittest.mock import Mock
 from Interactors.Exceptions.UserExistsException import UserExistsException
 from Interactors.InteractorFactory import InteractorFactory
 from Interactors.User.AddUserInteractor import AddUserInteractor
+from Interactors.User.LoginInteractor import LoginInteractor
 from UI.Handlers.Handler import Handler
+from UI.Handlers.Exceptions.SessionNotSetException import SessionNotSetException
+from UI.Handlers.Session.Session import Session
 from UI.Handlers.SignupHandler import SignupHandler
 from User import User
 
@@ -11,11 +14,20 @@ from User import User
 class TestSignupHandler(unittest.TestCase):
 
     def setUp(self):
-        self.__interactor = Mock(AddUserInteractor)
-        self.__interactor.execute = Mock(side_effect=self.__interactor_execute)
+        self.__add_user_interactor = Mock(AddUserInteractor)
+        self.__add_user_interactor.execute = Mock(side_effect=self.__interactor_execute)
+        self.__login_interactor = Mock(LoginInteractor)
         self.__interactor_factory = Mock(InteractorFactory)
-        self.__interactor_factory.create = Mock(return_value=self.__interactor)
+        self.__interactor_factory.create = Mock(side_effect=self.__interactor_factory_create)
+        self.__session = Mock(Session)
         self.__target = SignupHandler(self.__interactor_factory, None)
+        self.__target.session = self.__session
+
+    def __interactor_factory_create(self, interactor_type):
+        if interactor_type == "AddUserInteractor":
+            return self.__add_user_interactor
+        if interactor_type == "LoginInteractor":
+            return self.__login_interactor
 
     def __interactor_execute(self, user):
         if user.user_id == "alreadyexists":
@@ -26,13 +38,25 @@ class TestSignupHandler(unittest.TestCase):
 
     def test_get_create_page_sets_hash_provider(self):
         self.__target.get_page(self.__get_params())
-        self.assertTrue(self.__interactor.set_hash_provider.called)
+        self.assertTrue(self.__add_user_interactor.set_hash_provider.called)
 
-    def test_get_create_page_executes_interactor(self):
+    def test_get_create_page_executes_add_user_interactor(self):
         params = self.__get_params()
         user = self.__get_user(params["userid"], params["password"])
         self.__target.get_page(params)
-        self.__interactor.execute.assert_called_with(user)
+        self.__add_user_interactor.execute.assert_called_with(user)
+
+    def test_get_page_executes_login_interactor(self):
+        params = self.__get_params()
+        user = self.__get_user(params["userid"], params["password"])
+        self.__target.get_page(params)
+        self.__login_interactor.execute.assert_called_with(user)
+
+    def test_get_page_sets_login_session(self):
+        params = self.__get_params()                
+        user = self.__get_user(params["userid"], params["password"])
+        self.__target.get_page(params)
+        self.__session.set_value.assert_called_with("user_id", user.user_id)    
 
     def test_get_page_user_exists_returns_true(self):
         result = self.__target.get_page(self.__get_params("alreadyexists"))
@@ -41,6 +65,10 @@ class TestSignupHandler(unittest.TestCase):
     def test_get_page_user_does_not_exist_returns_true(self):
         result = self.__target.get_page(self.__get_params())
         self.assertTrue(result)
+
+    def test_get_page_without_setting_session_raises_session_not_set_exception(self):
+        target = SignupHandler(self.__interactor_factory, None)
+        self.assertRaises(SessionNotSetException, target.get_page, self.__get_params())        
 
     def __get_params(self, userid="user", password="password"):
         return {
