e661f1d2261eb5ba5b57d9e22650d686f4f37d75
David Wilson <david.tosslehoff@gmail.com>
2015-04-05 08:29:13 +0100

    split out login screen-specific js into own file.

--- UI/markup/js/script.js
+++ UI/markup/js/script.js
@@ -250,55 +250,6 @@ function toggleSortDirection(oldSortDir) {
     return oldSortDir == "asc" ? "desc": "asc";
 }
 
-function login() {
-    hideValidationMessages();
-    return loginPageAjax("/signin");
-}
-
-function loginDone(data) {
-	 if (data == false) return;
-
-	 if (data=="True") {
-		  showValidationSuccess("Login successful.");
-		  setTimeout(function() {navigate("/")}, 2000);
-	 } else {
-		  showValidationFailure("Login failed.");
-	 }
-	 
-}
-
-function newUser() {
-    hideValidationMessages();
-    return loginPageAjax("/signup");
-}
-
-function newUserDone(data) {
-    if (data == false) return;
-
-    if (data=="True") {
-        showValidationSuccess("Signed up successfully")
-    } else {
-        showValidationFailure("Error encountered while signing up")
-    }
-}
-
-function loginPageAjax(url) {
-    if (!validateLoginForm()) {
-		  var def = new $.Deferred();
-		  def.resolve(false);
-		  return def;
-	 }
-    return $.ajax({
-        url: url,
-        type: "POST",
-        data: {
-            userid: $("#userid").val(),
-            password: $("#password").val()
-        }
-    });
-}
-
-
 $(function() {
 	 var sessionStatus = $.cookie("session_status");
 	 if (sessionStatus == "1") setLoginText();	 
