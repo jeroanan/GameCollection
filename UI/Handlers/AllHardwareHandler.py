b12970b826383cdb6ee0fca69eecb9b3fafb4975
David Wilson <david.tosslehoff@gmail.com>
2015-04-09 07:59:58 +0100

    More work on login and redirect

--- UI/Handlers/AllHardwareHandler.py
+++ UI/Handlers/AllHardwareHandler.py
@@ -3,6 +3,8 @@ from UI.Handlers.Handler import Handler
 
 class AllHardwareHandler(Handler):
     def get_page(self, args):
+        self.check_session()
+        self.redirect_if_not_logged_in()
         interactor = self.interactor_factory.create("GetHardwareListInteractor")
         hardware = interactor.execute(sort_field="name", sort_direction="asc")
         return self.renderer.render("allhardware.html", hardware=hardware, title="All Hardware",
--- UI/Handlers/DeleteHardwareHandler.py
+++ UI/Handlers/DeleteHardwareHandler.py
@@ -1,10 +1,20 @@
-import cherrypy
 from UI.Handlers.Handler import Handler
 
 
 class DeleteHardwareHandler(Handler):
 
     def get_page(self, args):
+        self.check_session()
+        self.redirect_if_not_logged_in()
+
+        if not self.__validate_args(args):
+            return ""
+
         interactor = self.interactor_factory.create("DeleteHardwareInteractor")
-        interactor.execute(args.get("hardwareid", ""))
-        raise cherrypy.HTTPRedirect("/")
+        try:            
+            interactor.execute(args.get("hardwareid", ""))
+        except:
+            return ""
+
+    def __validate_args(self, args):
+        return "hardwareid" in args and args["hardwareid"] != ""
