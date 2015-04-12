ac23199f75b24ee639983c9cf7f3e09e801b11b1
David Wilson <david.tosslehoff@gmail.com>
2015-04-12 23:30:41 +0100

    SortHardwareHandler uses user id

--- UI/Handlers/SortHardwareHandler.py
+++ UI/Handlers/SortHardwareHandler.py
@@ -5,6 +5,9 @@ class SortHardwareHandler(AuthenticatedHandler):
     def get_page(self, args):
         super().get_page(args)
         interactor = self.interactor_factory.create("GetHardwareListInteractor")
-        hardware = interactor.execute(args.get("field", "name"), args.get("sortdir", ""))
+
+        hardware = interactor.execute(args.get("field", "name"), args.get("sortdir", ""), 
+                                      self.session.get_value("user_id"))
+
         return self.renderer.render("hardware.html", hardware=hardware, hw_sort_field=args.get("field", ""),
                                     hw_sort_dir=args.get("sortdir", ""))
