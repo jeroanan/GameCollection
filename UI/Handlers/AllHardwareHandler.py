551707e5b777cf4868b78878a39998f81b5404c2
David Wilson <david.tosslehoff@gmail.com>
2015-04-12 20:30:14 +0100

    The all hardware screen now only shows hardware for the current user

--- UI/Handlers/AllHardwareHandler.py
+++ UI/Handlers/AllHardwareHandler.py
@@ -3,9 +3,14 @@ from UI.Handlers.AuthenticatedHandler import AuthenticatedHandler
 
 class AllHardwareHandler(AuthenticatedHandler):
 
+    """The All Hardware page
+    Shows a list of all the hardware that the current user has in their collection.
+    param args: Seems not to be currently used. TODO: why not?
+    returns: The rendered all hardware page
+    """
     def get_page(self, args):
         super().get_page(args)
         interactor = self.interactor_factory.create("GetHardwareListInteractor")
-        hardware = interactor.execute(sort_field="name", sort_direction="asc")
+        hardware = interactor.execute(sort_field="name", sort_direction="asc", user_id=self.session.get_value("user_id"))
         return self.renderer.render("allhardware.html", hardware=hardware, title="All Hardware",
                                     hw_sort_field="name", hw_sort_dir="asc")
