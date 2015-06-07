c6eff392503aa80574fa7909948e075ce37f7422
jeroanan <david.tosslehoff@gmail.com>
2015-06-07 22:06:24 +0100

    more require

--- UI/markup/js/init.js
+++ UI/markup/js/init.js
@@ -1,20 +1,24 @@
 requirejs.config({
 	 baseUrl: '/static/js',
 	 paths: {
-		  jquery: ['//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min', 
-					  'jquery-2.1.4.min'],
+		  jquery: 'jquery-2.1.4.min',
 		  jqueryui: 'jquery-ui.min',
 		  jquerycookie: 'jquery.cookie-1.4.1.min',
-		  bootstrap: ['//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min',
-						  'bootstrap.min']
+		  'bootstrap.min': 'bootstrap.min'
 	 },
 	 shim: {
 		  jqueryui: ['jquery'],
 		  jquerycookie: ['jquery'],
-		  bootstrap: ['jquery'],
+		  'bootstrap.min': 'jquery',
 		  'script.min': ['jqueryui', 'jquerycookie'],
 		  'login.min': ['jquery', 'script.min']
 	 }
 });
 
-require(['bootstrap', 'script.min', 'login.min']);
+require(['jquery'], function () {
+	 require(['jqueryui', 'jquerycookie', 'bootstrap.min'], function () {
+		  require(['script.min'], function () {
+				require(['login.min']);
+		  });
+	 });
+});
