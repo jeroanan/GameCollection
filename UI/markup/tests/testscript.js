385199fd129ef14cf31e3c12ed2e7862b92785e1
jeroanan <david.tosslehoff@gmail.com>
2015-07-07 21:57:17 +0100

    JS unit testing

--- UI/markup/tests/testscript.js
+++ UI/markup/tests/testscript.js
@@ -12,7 +12,7 @@ QUnit.test('toggleSortDirection notDesc -> asc', function(assert) {
 	 assert.equal(toggleSortDirection('notDesc'), 'asc');
 });
 
-QUnit.test('getId -> id', function(assert) {
+QUnit.test('getIdJson -> id', function(assert) {
 	 var id = 'my id';
 	 $('#id').val(id);
 	 assert.equal(getIdJson().id, id);
@@ -38,3 +38,19 @@ QUnit.test('addNewNameDescription', function(assert) {
 	 assert.equal(name, nameElementValue);
 	 assert.equal(description, descriptionElementValue);
 });
+
+QUnit.test('appendText first line just includes that text.', function(assert) {
+	 var t = '';
+	 var a = 'some text';
+	 
+	 assert.equal(appendText(t, a), a);
+});
+
+QUnit.test('appendText inserts html breaks if adding subsequent lines.', function(assert) {
+	 var t = 'some text';
+	 var a = 'some more text';
+	 var expected = t + '<br />' + a;
+	 
+	 assert.equal(appendText(t, a), expected);
+});
+
