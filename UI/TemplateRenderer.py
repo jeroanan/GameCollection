118eae51304772cded618c0222db81d1d38420a0
Dave Wilson <davidwil@posteo.de>
2026-02-07 10:00:58 +0000

    fix error in templaterenderer

--- UI/TemplateRenderer.py
+++ UI/TemplateRenderer.py
@@ -1,12 +1,14 @@
+"""Renders templates using Jinja2."""
 from jinja2 import Environment, PackageLoader
 
 
 class TemplateRenderer(object):
+    """Renders templates using Jinja2."""
 
     def render(self, template, **args):
         template = self.__get_template(template)
         return template.render(args)
 
     def __get_template(self, template):
-        env = Environment(loader=PackageLoader("UI.WebServer", "markup"))
+        env = Environment(loader=PackageLoader("UI.web_server", "markup"))
         return env.get_template(template)
